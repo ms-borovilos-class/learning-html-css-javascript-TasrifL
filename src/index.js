88b56ffb07d40266d615ffe8ca67b77bf607df1c
TasrifL <119431077+TasrifL@users.noreply.github.com>
2022-11-30 14:40:59 +0000

    Hi

--- src/index.js
+++ src/index.js
@@ -18,19 +18,20 @@ function division(a, b){
     alert(a / b)
 }
 
-function calculator() {  var operation = prompt("Enter an operation")
-var a = promt("Enter your first number")
-var b = promt("Enter your second number")
+function calculator() {  
+   var operation = prompt("Enter an operation")
+   var a = promt("Enter your first number")
+   var b = promt("Enter your second number")
 
-if (operation = "addition")
-additon(a, b)
-else if (operation === "subtraction"){
+   if (operation === "addition") {
+    alert(addition(a, b)) 
+  } else if (operation === "subtraction") {
     alert(subtration(a, b))
-} else if (operation === "multiplication"){
+  } else if (operation === "multiplication") {
     alert(multiplication(a, b))  
-} else if (operation === "division"){
+  } else if (operation === "division") {
     alert(division(a, b))
-} else
-("Ivalid operation")   
+  } else {
+    alert("Ivalid operation")   
+  }
 }
-  
